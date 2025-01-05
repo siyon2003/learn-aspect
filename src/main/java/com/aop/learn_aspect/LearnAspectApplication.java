841b8f66d08ecc89d17b0310e9148ae4d39a4f95
siyon <siyon2003@gmail.com>
2025-01-05 18:59:42 +0530

    Initialized gradle spring boot project

---
+++ src/main/java/com/aop/learn_aspect/LearnAspectApplication.java
@@ -0,0 +1,13 @@
+package com.aop.learn_aspect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnAspectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnAspectApplication.class, args);
+	}
+
+}
